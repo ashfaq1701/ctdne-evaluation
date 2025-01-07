de2028075cefd6ff29444783ca6c7629a841408b
ashfaq1701 <ashfaq.aws@gmail.com>
2025-01-07 19:27:36 +0000

    Updated scripts

--- run_tests_weighted_node2vec.bash
+++ run_tests_weighted_node2vec.bash
@@ -15,12 +15,3 @@ python index.py --dataset ia_enron_employees --walk_bias Exponential --initial_e
 python index.py --dataset ia_radoslaw_email --walk_bias Exponential --initial_edge_bias Uniform --edge_operator hadamard --weighted_node2vec
 python index.py --dataset soc_sign_bitcoin_alpha --walk_bias Exponential --initial_edge_bias Uniform --edge_operator hadamard --weighted_node2vec
 python index.py --dataset wiki_elections --walk_bias Exponential --initial_edge_bias Uniform --edge_operator hadamard --weighted_node2vec
-
-python index.py --dataset fb_forum --walk_bias Exponential --initial_edge_bias Uniform
-python index.py --dataset ia_contact --walk_bias Exponential --initial_edge_bias Uniform
-python index.py --dataset ia_contacts_hypertext_2009 --walk_bias Exponential --initial_edge_bias Uniform
-python index.py --dataset ia_email_eu --walk_bias Exponential --initial_edge_bias Uniform
-python index.py --dataset ia_enron_employees --walk_bias Exponential --initial_edge_bias Uniform
-python index.py --dataset ia_radoslaw_email --walk_bias Exponential --initial_edge_bias Uniform
-python index.py --dataset soc_sign_bitcoin_alpha --walk_bias Exponential --initial_edge_bias Uniform
-python index.py --dataset wiki_elections --walk_bias Exponential --initial_edge_bias Uniform
